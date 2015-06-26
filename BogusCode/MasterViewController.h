991dd5e64084165847b4a7f0bd30a53a47ea2526
Rob Huebner <huebnerob@gmail.com>
2015-06-26 11:24:06 -0400

    Initial Commit

---
+++ BogusCode/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  BogusCode
+//
+//  Created by Huebner, Rob on 6/26/15.
+//  Copyright (c) 2015 Vimeo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
